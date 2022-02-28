aecdf14029f57ba45ec735101667e46a6165353f
Marcos Lemos <marcospaulolemos0@gmail.com>
2022-02-28 16:47:01 -0300

    worker example

---
+++ src/main/java/com/marcos/vertex/vertex_starter_study/worker/WorkerExemple.java
@@ -0,0 +1,47 @@
+package com.marcos.vertex.vertex_starter_study.worker;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkerExemple extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(WorkerExemple.class);
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.deployVerticle(WorkerExemple.class.getName());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.deployVerticle(new WorkerVerticle(), new DeploymentOptions()
+      .setWorker(true)
+      .setWorkerPoolSize(1)
+      .setWorkerPoolName("my-worker-verticle"));
+    startPromise.complete();
+    executeBlockingCode();
+  }
+
+  private void executeBlockingCode() {
+    vertx.executeBlocking(event -> {
+      LOG.debug("Executing blocking code");
+      try {
+        Thread.sleep(5000);
+        event.complete();
+      } catch (InterruptedException e) {
+        LOG.debug("Failed: ", e);
+        event.fail(e);
+      }
+    }, result -> {
+      if(result.succeeded()) {
+        LOG.debug("Blocking call done.");
+      } else {
+        LOG.debug("Blocking call failed due to:", result.cause());
+      }
+    });
+  }
+}
